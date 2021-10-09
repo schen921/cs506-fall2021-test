c4ed46fa615c3907589bac479551609738c3949f
schen921 <48606083+schen921@users.noreply.github.com>
2021-10-09 17:31:33 -0500

    Update trading_Naive.java

--- trading_Naive.java
+++ trading_Naive.java
@@ -1,5 +1,5 @@
 public class trading_Naive {
-    public int maxProfit(int prices[]) {
+    public static int maxProfit(int prices[]) {
         int maxprofit = 0;
         // enumerate all possible buy dates
         for (int buyDate = 0; buyDate < prices.length - 1; buyDate++) {
