21f3754bd6cb5b0ac3569ff62097b05bebc15a50
schen921 <48606083+schen921@users.noreply.github.com>
2021-10-09 17:30:28 -0500

    new test method

--- Test.java
+++ Test.java
@@ -1,9 +1,17 @@
 class Test {
-	public static boolean test() {
-		int[] nums = new int[]{2,3,5,0,1,8,7,2,3,1,0,9};
-		int solution = 9;
+	public static boolean test(int[] nums) {
+		int sol = solution(nums);
 		int testNaive = trading_Naive.maxProfit(nums);
 		int testOnepass = trading_Onepass.maxProfit(nums);
 		return testNaive == solution && testOnepass == solution;
+	}
+	
+	private static int solution(int[] nums) {
+		int maxCur = 0, maxSoFar = 0;
+		for(int i = 1; i < prices.length; i++) {
+		    maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+		    maxSoFar = Math.max(maxCur, maxSoFar);
+		}
+		return maxSoFar;
 	}
 }
